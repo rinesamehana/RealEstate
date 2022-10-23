fd1ba65c65a28c4c2714c84dd892b8adb9eb1b2a
RinaVeseli <93445391+RinaVeseli@users.noreply.github.com>
2022-10-23 17:42:35 +0200

    up

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -42,9 +42,9 @@ namespace API.Middleware
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
-                var json = JsonSerializer.Serialize(response, options);
+                var jsonn = JsonSerializer.Serialize(response, options);
 
-                await context.Response.WriteAsync(json);
+                await context.Response.WriteAsync(jsonn);
             }
         }
     }
