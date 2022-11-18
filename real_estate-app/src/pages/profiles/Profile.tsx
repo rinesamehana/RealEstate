659b7d83325496debf0147b56f9837ca7151d715
rinesamehana <93328298+rinesamehana@users.noreply.github.com>
2022-11-18 18:13:37 +0100

    update

--- real_estate-app/src/pages/profiles/Profile.tsx
+++ real_estate-app/src/pages/profiles/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 import ProfileHeader from './ProfileHeader'
 
 export default function Profile() {
   return (
     <div>
+        <Navbar />
         <ProfileHeader/>
     </div>
   )
