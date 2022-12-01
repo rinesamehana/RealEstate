c384a410a13f96f1f274b093a9a879a473f71709
RinaVeseli <93445391+RinaVeseli@users.noreply.github.com>
2022-12-01 21:52:17 +0100

    asdas

--- real_estate-app/src/pages/profiles/ProfileHeader.tsx
+++ real_estate-app/src/pages/profiles/ProfileHeader.tsx
@@ -17,7 +17,7 @@ export default observer( function ProfileHeader({profile} :Props){
             <Grid.Column width={12}>
                 <Item.Group>
                     <Item>
-                        <Item.Image avatar size='small' src={profile.image || 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='}/>
+                        <Item.Image avatar size='small' src={profile.image || '/assets/user.png'}/>
                             <Item.Content verticalAlign='middle'>
                                 <Header as='h1' content={profile.displayName}/>
                             </Item.Content>
