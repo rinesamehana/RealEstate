ce8775247c6fa70709440d4aa4bd2a985164d222
RinaVeseli <93445391+RinaVeseli@users.noreply.github.com>
2022-12-01 16:12:11 +0100

    sda

--- real_estate-app/src/pages/profiles/ProfilePhotos.tsx
+++ real_estate-app/src/pages/profiles/ProfilePhotos.tsx
@@ -9,8 +9,6 @@ interface Props {
     profile: Profile;
 }
 
-
-
 export default observer(function ProfilePhotos({ profile }: Props) {
     const { profileStore: { isCurrentUser, uploadPhoto, 
             uploading, loading, setMainPhoto, deletePhoto } } = useStore();
@@ -37,7 +35,8 @@ export default observer(function ProfilePhotos({ profile }: Props) {
                 <Grid.Column width={16}>
                     <Header floated='left' icon='image' content='Photos' />
                     {isCurrentUser && (
-                        <Button floated='right' basic content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+                        <Button floated='right' basic
+                            content={addPhotoMode ? 'Cancel' : 'Add Photo'}
                             onClick={() => setAddPhotoMode(!addPhotoMode)}
                         />
                     )}
@@ -81,41 +80,3 @@ export default observer(function ProfilePhotos({ profile }: Props) {
         </Tab.Pane>
     )
 })
-
-
-
-// export default observer(function ProfilePhotos({ profile }: Props) {
-//     const { profileStore: { isCurrentUser} } = useStore();
-//      const [addPhotoMode, setAddPhotoMode] = useState(false);
-
-//     return (
-//         <Tab.Pane>
-//             <Grid>
-//                <Grid.Column  width={10}>
-//                <Header floated='left'  icon='image' content='Photos' />
-//                {isCurrentUser && (
-//                      <Button floated='right' basic content={addPhotoMode ? 'Cancel' : 'Add Photo'}
-//                             onClick={() => setAddPhotoMode(!addPhotoMode)}
-//                        />)}
-//                </Grid.Column>
-//                <Grid.Column width={16}>
-//                   {addPhotoMode? (
-//                     <p>Photo widget goes here</p>
-//                    ):( 
-                   
-//                    <Card.Group itemsPerRow={5}>
-//                     {profile.photos?.map(photo => (
-//                         <Card key={photo.id}>
-//                             <Image src={photo.url} />
-//                         </Card>
-//                     ))}
-//                 </Card.Group>
-                    
-//                    )}
-//                </Grid.Column>
-//             </Grid>
-                    
-                           
-//                              </Tab.Pane>
-//     )
-// })
